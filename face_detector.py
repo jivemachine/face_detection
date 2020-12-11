c6c79712bed992372a5c5e78e025e34119866bbc
jivemachine <the.jeremy.cobb@gmail.com>
2020-12-11 15:41:20 -0600

    wrote face detection script to work on photos

--- face_detector.py
+++ face_detector.py
@@ -3,7 +3,7 @@ import cv2
 # initializing our model
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
 # pulling our image
-img = cv2.imread("test.jpeg")
+img = cv2.imread("test_image.jpeg")
 # converting our image to grayscale
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 # detecting faces
